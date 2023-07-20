988846eb39d7c306b39f33e3d2c6781b6409df71
manindraverma <53371899+manindraverma@users.noreply.github.com>
2023-07-20 11:22:30 +0530

    Create builtInFunction.js

---
+++ builtInFunction.js
@@ -0,0 +1,17 @@
+//eval()
+console.log(eval('2+3'));
+//parseInt ==parses the string argument and extract the Integer
+console.log(parseInt('123a12'));
+console.log(parseFloat('3.14a'));
+
+//isNan-checks whether the passed argument is number or not
+console.log(isNaN(23));
+//isFinite-it determines whether a passed value is finite or not
+console.log(isFinite(NaN))
+//Number()-converts the value of an  object into a number
+console.log(Number('123'))//here it convert value of string object into a number
+
+
+//String()-convert the value of an object into a string and also it is the constructor of string object
+console.log((String(12)));
+console.log(typeof(String(12)));
